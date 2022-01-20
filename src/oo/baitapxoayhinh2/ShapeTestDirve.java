27cb7e28aa75c95d1984077bdba586b0981490f7
hoang304 <hoangyor304@gmail.com>
2022-01-20 14:45:45 +0700

    bai tap xoay hinh

--- src/oo/baitapxoayhinh2/ShapeTestDirve.java
+++ src/oo/baitapxoayhinh2/ShapeTestDirve.java
@@ -3,17 +3,17 @@ package oo.baitapxoayhinh2;
 public class ShapeTestDirve {
     public static void main(String[] args) {
         System.out.println("Hình Tam Giác");
-        Triangle triangle = new Triangle("mùa xuân");
+        Triangle triangle = new Triangle("ting ting ting");
         triangle.rotate();
         triangle.playSound();
 
         System.out.println("Hình Vuông");
-        Square square = new Square("mùa hè");
+        Square square = new Square("ten ten ten");
         square.rotate();
         square.playSound();
 
         System.out.println("Hình Tròn");
-        Circle circle = new Circle("Mùa đông");
+        Circle circle = new Circle("tang tang tang");
         circle.rotate();
         circle.playSound();
     }
